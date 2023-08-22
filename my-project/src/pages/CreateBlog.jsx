5831526b054176647c8d0e68a83a34adab10353c
saahil <saahil.sl.arnd@gmail.com>
2023-08-22 23:59:26 +0530

    react icons

--- my-project/src/pages/CreateBlog.jsx
+++ my-project/src/pages/CreateBlog.jsx
@@ -2,11 +2,14 @@
 
 export const CreateBlog = () => {
   return (
-    <div className=" w-[60vw] mx-auto mt-10 "> 
+    <div className="w-[60vw] mx-auto mt-10 "> 
         <h1 className="text-xl font-bold text-center">CreateBlog</h1>
           <form className=" flex flex-col gap-2">
             <label className="font-semibold text-lg" htmlFor="title">Title:</label>
             <input type="text" name="title" placeholder="Enter the blog title" className="px-3 py-2 rounded-md outline-none border-2 border-gray-300" />
+            <label className="font-semibold text-lg" htmlFor="description">Description:</label>
+            <textarea placeholder="Enter the content" name="" id="" cols="30" rows="10" className="px-3 py-2  rounded-md outline-none border-2 border-gray-300"></textarea>
+            <button type="submit" className="bg-purple-300 hover:bg-purple-500 rounded-md text-lg text-white py-2 font-semibold">Submit </button>
           </form>
     </div>    
        
--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -1,7 +1,23 @@
+import {AiFillDelete} from "react-icons/ai"
+import {MdOutlineModeEditOutline} from "react-icons/md"
 
 export const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+
+  return( 
+     <div className="mt-10">
+    <div className="w-[40vw]  border-spacing-2 mx-auto">
+<div className="flex justify-end  gap-3 m-3">
+<AiFillDelete className="text-gray-400 hover:text-red-500"/>
+<MdOutlineModeEditOutline className="text-gray-400 hover:text-red-500"/>
+</div>
+<h2 className="text-lg font-bold">
+  Lorem ipsum dolor sit, amet consectetur adipisicing.
+</h2>
+<h3>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nesciunt dolorem dddddddddddd  ddddddsfs swfsc
+</h3>
+    </div>
+  </div>
+ );
+};
 export default Home
